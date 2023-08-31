78a27a4e0635b34fd758573be6eba41e2bef4aa3
Austin Rooks <Austionian@users.noreply.github.com>
2023-08-31 10:15:12 -0500

    only show 4 posts max on index

--- src/routes/root.rs
+++ src/routes/root.rs
@@ -5,8 +5,14 @@ use std::sync::Arc;
 // basic handler that responds with a static string
 pub async fn root(State(state): State<Arc<AppState>>) -> Html<String> {
     let mut context = tera::Context::new();
-    // Eventually take a slice of posts to only show 5 or so.
-    context.insert("posts", &state.posts);
+    const NUMBER_OF_POSTS: usize = 4;
+
+    if &state.posts.len() > &NUMBER_OF_POSTS {
+        context.insert("posts", &state.posts[..NUMBER_OF_POSTS]);
+    } else {
+        context.insert("posts", &state.posts);
+    }
+
     match TEMPLATES.render("index.html", &context) {
         Ok(s) => Html(s),
         Err(_) => Html("<html><body>Error</body></html>".to_string()),
