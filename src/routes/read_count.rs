33ce60b8658efc1357fa2cc324d239930582752f
Austin Rooks <Austionian@users.noreply.github.com>
2023-10-14 12:55:14 -0500

    refine view count on posts

--- src/routes/read_count.rs
+++ src/routes/read_count.rs
@@ -3,11 +3,12 @@ use axum::response::{Html, IntoResponse};
 
 #[derive(serde::Deserialize)]
 struct D1Post {
-    id: uuid::Uuid,
     read_count: u32,
 }
 
-/// Handler to the number of times a blog post has been read
+/// Handler to the number of times a blog post has been read. Could maybe call
+/// this from the client, but leaving here to potentially use as a api key
+/// protected proxy in the future. Would need to update the worker to only return the number.
 pub async fn read_count(Path(post_id): Path<String>) -> impl IntoResponse {
     let response = reqwest::get(format!(
         "https://worker-rust.austin-e33.workers.dev/{}",
